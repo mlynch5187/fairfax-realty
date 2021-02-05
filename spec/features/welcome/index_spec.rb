da5c98c03eef5bd5fa5539621d9a51891ce7d07e
Michael Alex Lynch <mlynch5187@gmail.com>
2021-02-04 17:43:47 -0700

    Update image path to point to repo

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe "welcome index page", type: :feature do
 
     visit "/"
 
-    expect(page).to have_css("img[src*='assets/images/lucie_realtor.jpg']")
+    expect(page).to have_css("img[src*='https://raw.githubusercontent.com/mlynch5187/fairfax-realty/main/app/assets/images/lucie_realtor.jpg']")
   end
 
   it "has links to each section" do
