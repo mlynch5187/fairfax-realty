15decc84f50eae33f9fba3df3eb3eb96e372cc97
Michael Alex Lynch <mlynch5187@gmail.com>
2021-02-07 09:42:07 -0700

    Add create sessions method

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,4 +2,16 @@ class SessionsController < ApplicationController
 
   def new; end
 
+  def create
+    user = User.find_by(email: params[:email])
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.name}!"
+      redirect_by_role
+    else
+      flash[:error] = "Sorry, your credentials are bad."
+      render :new
+    end
+  end
+
 end
