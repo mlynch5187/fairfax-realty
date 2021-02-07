103e4ebd7f469915d70a399cecf5f63875089181
Michael Alex Lynch <mlynch5187@gmail.com>
2021-02-07 05:57:48 -0700

    Bolster admin login spec

--- spec/features/admin/login_spec.rb
+++ spec/features/admin/login_spec.rb
@@ -6,19 +6,18 @@ RSpec.describe("Admin Merchants Index Page") do
       @admin = User.create(email: "admin@example.com",
                            password: "123456",
                            role: 1)
-
-                  @maple_street = Listing.create(address: '9106 Maple Way',
-                                                 city: 'Catonsville',
-                                                 state: 'MD',
-                                                 zip: 21228,
-                                                 image: 'https://cdn.vox-cdn.com/thumbor/XUdUk_rKJ8WDTd0zksnnicYDwbY=/0x0:3760x2500/1200x800/filters:focal(1580x950:2180x1550)/cdn.vox-cdn.com/uploads/chorus_image/image/65135516/shutterstock_349464740.0.jpg',
-                                                 description: "A beautiful home!")
-                  @shady_grove = Listing.create(address: '3167 Shady Grove Road',
-                                                city: 'Columbia',
-                                                state: 'MD',
-                                                zip: 21045,
-                                                image: 'https://i0.wp.com/movingtips.wpengine.com/wp-content/uploads/2020/08/new-townhouse.jpg?fit=1024%2C684&ssl=1',
-                                                description: "Located near Lake Centennial. The autumn colors are gorgeous!")
+      @maple_street = Listing.create(address: '9106 Maple Way',
+                                     city: 'Catonsville',
+                                     state: 'MD',
+                                     zip: 21228,
+                                     image: 'https://cdn.vox-cdn.com/thumbor/XUdUk_rKJ8WDTd0zksnnicYDwbY=/0x0:3760x2500/1200x800/filters:focal(1580x950:2180x1550)/cdn.vox-cdn.com/uploads/chorus_image/image/65135516/shutterstock_349464740.0.jpg',
+                                     description: "A beautiful home!")
+      @shady_grove = Listing.create(address: '3167 Shady Grove Road',
+                                    city: 'Columbia',
+                                    state: 'MD',
+                                    zip: 21045,
+                                    image: 'https://i0.wp.com/movingtips.wpengine.com/wp-content/uploads/2020/08/new-townhouse.jpg?fit=1024%2C684&ssl=1',
+                                    description: "Located near Lake Centennial. The autumn colors are gorgeous!")
     end
 
     it "I can login to access the admin dashboard" do
@@ -30,6 +29,11 @@ RSpec.describe("Admin Merchants Index Page") do
       click_button "Login"
 
       expect(current_path).to eq("/admin/dashboard")
+      expect(page).to have_content("You have logged in")
+      expect(page).to have_content("9106 Maple Way")
+      expect(page).to have_content("3167 Shady Grove")
+      expect(page).to have_link("New Listing")
+      expect(page).to have_link("Edit Listing")
     end
   end
 end
