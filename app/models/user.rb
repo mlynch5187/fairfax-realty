7d45ac8930afc5462715a8ae5d4309c3de603eda
Michael Alex Lynch <mlynch5187@gmail.com>
2021-02-07 06:11:09 -0700

    Add users table to DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates_presence_of :email, :password
+  validates_presence_of :email
   validates_presence_of :password, on: :create
   validates :email, uniqueness: true
 
