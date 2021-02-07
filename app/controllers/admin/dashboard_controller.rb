0d98a6de00c8f9f66e0dad8b8a11df3c774a4e4c
Michael Alex Lynch <mlynch5187@gmail.com>
2021-02-07 06:20:32 -0700

    Create admin namespace, admin login route, and admin dashboard controller

---
+++ app/controllers/admin/dashboard_controller.rb
@@ -0,0 +1,5 @@
+class Admin::DashboardController < ApplicationController
+
+  def login; end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
   get '/listings', to:'listings#index'
   get '/listings/:listing_id', to:'listings#show'
-  # get '/testimonials'; to 'testimonials'
+
+  namespace :admin do
+    get "/login", to: 'dashboard#login'
+  end
 end
