62be5a3d2acca76fdf4b7bd76b33d1784852954b
Michael Alex Lynch <mlynch5187@gmail.com>
2021-02-07 09:40:40 -0700

    Create sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   get '/', to: 'welcome#index'
-  get "/login", to: 'welcome#login'
   get '/listings', to:'listings#index'
   get '/listings/:listing_id', to:'listings#show'
 
   namespace :admin do
     get "/dashboard", to: 'dashboard#index'
     post "/dashboard", to: 'dashboard#index'
+
+    # get "/login", to: "sessions#new"
+    # post "/login", to: "sessions#create"
+    # get "/logout", to: "sessions#destroy"
   end
 end
