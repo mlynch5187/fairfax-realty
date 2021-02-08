e4f5be369abdf0067d715133ca9069ff4a87ab6b
Michael Alex Lynch <mlynch5187@gmail.com>
2021-02-08 10:03:32 -0700

    Add admin/listings/id/edit route

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,14 @@ Rails.application.routes.draw do
   get '/listings/:listing_id', to:'listings#show'
   post '/listings', to: 'listings#create'
 
-  get "/login", to: "sessions#new"
-  post "/login", to: "sessions#create"
-  get "/logout", to: "sessions#destroy"
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 
   namespace :admin do
-    get "/dashboard", to: 'dashboard#index'
-    post "/dashboard", to: 'dashboard#index'
-    get "/listings/new", to: 'listings#new'
+    get '/dashboard', to: 'dashboard#index'
+    post '/dashboard', to: 'dashboard#index'
+    get '/listings/new', to: 'listings#new'
+    get '/listings/:listing_id/edit', to: 'listing#edit'
   end
 end
