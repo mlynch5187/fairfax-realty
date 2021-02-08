051e1a9c2d4ac8942528c282e8c0114bead624dd
Michael Alex Lynch <mlynch5187@gmail.com>
2021-02-08 11:26:47 -0700

    Remove unused application controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,15 +5,10 @@ class ApplicationController < ActionController::Base
                 :current_user?,
                 :current_admin?,
                 :require_admin,
-                :require_not_admin,
                 :logged_in?
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def current_user?
-    current_user && current_user.user?
   end
 
   def current_admin?
@@ -22,10 +17,6 @@ class ApplicationController < ActionController::Base
 
   def require_admin
     render file: "/public/404" unless current_admin?
-  end
-
-  def require_not_admin
-    render file: "/public/404" if current_admin?
   end
 
   def logged_in?
