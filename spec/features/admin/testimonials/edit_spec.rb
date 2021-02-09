b54881a8a6092d21cbf10d5a47f9b2a70810ac31
Michael Alex Lynch <mlynch5187@gmail.com>
2021-02-09 12:40:08 -0700

    Write admin testimonial edit spec

---
+++ spec/features/admin/testimonials/edit_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin Testimonials Edit Page', type: :feature do
+  describe 'As a logged in admin' do
+    before :each do
+      @admin = User.create(email: "admin@example.com",
+                           password: "123456",
+                           role: 0)
+                           @jill_hartford = Testimonial.create(description: 'Lucie is kind and professional. I highly recommend her!',
+                                                               author: "Jill Hartford")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+
+      visit '/admin/dashboard'
+
+      expect(page).to have_content("Lucie is kind and professional. I highly recommend her!")
+    end
+
+    it 'I can edit an existing testimonial' do
+
+      click_link "Edit Testimonial"
+
+      fill_in :description, with: "I made a new testimonial to replace the old one"
+      fill_in :author, with: "Jill Hartfordson"
+
+      click_button "Edit Testimonial"
+
+      new_testimonial = Testimonial.last
+
+      expect(current_path).to eq('/admin/dashboard')
+      expect(page).to have_content("I made a new testimonial to replace the old one")
+      expect(new_testimonial.description).to eq("I made a new testimonial to replace the old one")
+      expect(new_testimonial.city).to eq("Jill Hartfordson")
+    end
+  end
+end
