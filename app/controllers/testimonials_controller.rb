6d85ce089bec6827824590724c1f03ad1fe2f8d2
Michael Alex Lynch <mlynch5187@gmail.com>
2021-02-09 12:07:44 -0700

    Add create method to testimonials controller

--- app/controllers/testimonials_controller.rb
+++ app/controllers/testimonials_controller.rb
@@ -3,4 +3,14 @@ class TestimonialsController < ApplicationController
   def index
     @testimonials = Testimonial.all
   end
+
+  def create
+    testimonial = Testimonial.create(testimonial_params)
+    if testimonial.save
+      redirect_to "/admin/dashboard"
+    else
+      redirect_to "/admin/testimonials/new"
+      flash[:error] = testimonial.errors.full_messages.to_sentence
+    end
+  end
 end
