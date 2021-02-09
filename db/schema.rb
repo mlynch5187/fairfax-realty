ecec8c364dba4d3a0664b2eff48c725ea9c1dd9f
Michael Alex Lynch <mlynch5187@gmail.com>
2021-02-09 10:58:44 -0700

    Integrate testimonials table with columns for description and author

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_07_130807) do
+ActiveRecord::Schema.define(version: 2021_02_09_175303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 2021_02_07_130807) do
     t.string "zip"
     t.string "image"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "testimonials", force: :cascade do |t|
+    t.text "description"
+    t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
