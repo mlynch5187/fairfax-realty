5a2ead50c2d35a4fa54c59757bcbe2344a814611
Michael Alex Lynch <mlynch5187@gmail.com>
2021-02-10 21:14:51 -0700

    Add consultations route

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+  get 'consultations', to: 'consultations#index'
 
   namespace :admin do
     get '/dashboard', to: 'dashboard#index'
--- spec/features/consultations/index_spec.rb
+++ spec/features/consultations/index_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe "Consulations Index Page", type: :feature do
   describe 'As a visitor' do
-    before :each do
 
     it "shows a section to book consultations" do
 
