5c0be55d163401e2816c05a5974ec58a322fb723
Michael Alex Lynch <mlynch5187@gmail.com>
2021-02-12 11:50:06 -0700

    Create first admin appointments index spec

---
+++ spec/features/admin/appointments/index_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin Appointments Index Page', type: :feature do
+  describe 'As a logged in admin' do
+    before(:each) do
+      @admin = User.create(email: "admin@example.com",
+                           password: "123456",
+                           role: 0)
+
+      @appointment_1 = @admin.appointments.create(email: 'interestedclient@mail.com',
+                                          time: '6:00')
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+
+      visit '/admin/dashboard'
+    end
+
+    it 'I can view all of my appointments' do
+      click_link "My Appointments"
+
+      expect(current_path).to eq('/admin/appointments')
+      expect(page).to have_content("Appointment with interestedclient@mail.com")
+      expect(page).to have_content("6:00")
+      expect(@appointment_1.time).to eq("6:00")
+      expect(@appointment_1.email).to eq("interestedclient@mail.com")
+    end
+  end
+end
