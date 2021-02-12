d24e774aef61e8e768d7fa90c6bd9699c27f055a
Michael Alex Lynch <mlynch5187@gmail.com>
2021-02-12 11:58:59 -0700

    Add date column to appointments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_12_183816) do
+ActiveRecord::Schema.define(version: 2021_02_12_185814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_02_12_183816) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "date"
     t.index ["user_id"], name: "index_appointments_on_user_id"
   end
 
